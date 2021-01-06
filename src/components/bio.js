8c533b04e18fa9916810325b3fc98d6b189822de
zhangyouxin <zhangyouxingg@126.com>
2021-01-06 16:51:29 +0800

    在 bio 中添加 GitHub 链接

--- src/components/bio.js
+++ src/components/bio.js
@@ -59,6 +59,8 @@ const Bio = () => {
           <a href={`https://twitter.com/${social?.twitter || ``}`}>推特</a>
           {` `}
           <a href={`https://weibo.com/u/1674831423`} >微博</a>
+          {` `}
+          <a href={`https://github.com/zhangyouxin`} >GitHub</a>
         </p>
       )}
     </div>
